7c18ef1fd48a357da4465cb6749e5af01cb05a28
Marcus Grönnå <mgronna@gmail.com>
2026-08-24 16:49:30 +0200

    test(books-page): add unit tests for BooksPage component initialization and API call

--- frontend/src/app/features/books/books-page/books-page.spec.ts
+++ frontend/src/app/features/books/books-page/books-page.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BooksPage } from './books-page';
+import { BooksApi } from '../books-api';
+import { of } from 'rxjs';
 
 describe('BooksPage', () => {
   let component: BooksPage;
   let fixture: ComponentFixture<BooksPage>;
+  let booksApi: jasmine.SpyObj<BooksApi>;
 
   beforeEach(async () => {
+    booksApi = jasmine.createSpyObj<BooksApi>('BooksApi', ['getBooks']);
+    booksApi.getBooks.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
-      imports: [BooksPage]
-    })
-    .compileComponents();
+      imports: [BooksPage],
+      providers: [{ provide: BooksApi, useValue: booksApi }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BooksPage);
     component = fixture.componentInstance;
@@ -19,5 +25,9 @@ describe('BooksPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should request books when initialized', () => {
+    expect(booksApi.getBooks).toHaveBeenCalledOnceWith();
   });
 });
