de98248700994011fe8417cbc7ec7d79310d88f5
Marcus Grönnå <mgronna@gmail.com>
2026-08-24 14:00:08 +0200

    fix(backend): adjust formatting of book entries in BooksController

--- backend/src/BookQuoteManager.Api/Features/Books/BooksController.cs
+++ backend/src/BookQuoteManager.Api/Features/Books/BooksController.cs
@@ -7,23 +7,23 @@ namespace BookQuoteManager.Api.Features.Books;
 public sealed class BooksController : ControllerBase
 {
     private static readonly BookResponse[] Books =
-        [
+    [
         new(
             1,
-              "Clean Code",
-              "Robert C. Martin",
-              new DateOnly(2008, 8, 1)),
-          new(
-              2,
-              "The Pragmatic Programmer",
-              "David Thomas and Andrew Hunt",
-              new DateOnly(1999, 10, 20)),
-          new(
-              3,
-              "Refactoring",
-              "Martin Fowler",
-              new DateOnly(1999, 7, 8))
-        ];
+            "Clean Code",
+            "Robert C. Martin",
+            new DateOnly(2008, 8, 1)),
+        new(
+            2,
+            "The Pragmatic Programmer",
+            "David Thomas and Andrew Hunt",
+            new DateOnly(1999, 10, 20)),
+        new(
+            3,
+            "Refactoring",
+            "Martin Fowler",
+            new DateOnly(1999, 7, 8))
+    ];
 
     [HttpGet]
     public ActionResult<IReadOnlyList<BookResponse>> GetBooks()
